ec5d6da08163db717c64f6a12d3ac0d2f5d009c9
Sergio López <sergiolf96@gmail.com>
2018-02-13 12:28:41 +0100

    Missing connection between React and Redux solved.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './../assets/scss/main.scss';
+import {connect} from 'react-redux';
 
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <h2 id="heading">Hello ReactJS</h2>
@@ -11,3 +12,11 @@ export default class App extends React.Component {
 
 }
 
+
+function mapStateToProps(state) {
+    return {
+        initialState: state.initialState,
+    };
+}
+
+export default connect(mapStateToProps)(App);
