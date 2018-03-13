c58d03ab38577fe548439e39e5a1a4ede184a059
Sergio López <sergiolf96@gmail.com>
2018-03-13 14:04:09 +0100

    Details on router requests

--- videoServer/requests.js
+++ videoServer/requests.js
@@ -1,7 +1,27 @@
 import {Router} from 'express';
+import {server} from "./server";
+
+let fs = require('fs');
+let HLSServer = require('hls-server');
 
 export let router = Router();
 
 router.get('/', () => {
-    console.log("Holiwi");
+
+});
+
+router.get('/:videoName', (req, res) => {
+
+    let path = 'streams/' + req.params.videoName;
+
+    if (!fs.existsSync(path)) {
+        console.log("No existe");
+        return;
+    }
+
+    let hls = new HLSServer(server, {
+        path: '/' + req.params.videoName,     // Base URI to output HLS streams
+        dir: 'streams/' + req.params.videoName + '/playlist.m3u8'  // Directory that input files are stored
+    });
+
 });
--- videoServer/server.js
+++ videoServer/server.js
@@ -1,14 +1,19 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 import {router} from "./requests";
 import {createServer} from 'http';
 
 // Creating router for handling video requests
 let app = express();
-let server = createServer(app);
+export let server = createServer(app);
 let port = process.env.VIDEO_SERVER_PORT || '8000';
 
 //Port
 app.set('port', port);
+
+//QueryURL
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //Router listening
 server.listen(port, function() {
